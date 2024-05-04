a2ad373c1168cfce8b1a1dc0985689093e2fa6a0
Chițu Ioan <131875575+ionut200328@users.noreply.github.com>
2024-05-04 19:35:06 +0300

    Added items count

--- src/app/services/dessert.service.ts
+++ src/app/services/dessert.service.ts
@@ -31,6 +31,30 @@ export class DessertService {
       description: 'A sweet dessert consisting of one or more layers',
       image: 'assets/cheesecake.jpg',
       price: 4.00
+    },
+    {
+      title: 'Caramel Cafe',
+      description: 'Espresso with steamed milk, caramel syrup, and whipped cream',
+      image: 'assets/caramel-cake.jpg',
+      price: 4.50
+    },
+    {
+      title: 'Cupcake',
+      description: 'A small cake designed to serve one person',
+      image: 'assets/cupcake.jpg',
+      price: 2.00
+    },
+    {
+      title: 'Chocolate Cake',
+      description: 'A rich, moist cake with a chocolate flavor',
+      image: 'assets/chocolate-cake.jpg',
+      price: 3.50
+    },
+    {
+      title: 'Cheesecake',
+      description: 'A sweet dessert consisting of one or more layers',
+      image: 'assets/cheesecake.jpg',
+      price: 4.00
     }
   ];
 
